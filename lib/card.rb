60c00c00ca23396f9b5dbd0aa02ad744f29ae2aa
Genevieve Neely <genevieve.hood@gmail.com>
2020-09-28 12:43:54 -0700

    wave 2

--- lib/card.rb
+++ lib/card.rb
@@ -16,7 +16,18 @@ class Card
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    if [1, 11, 12, 13].include?(value)
+      return "#{convert_facecard_value} of #{suit.to_s}"
+    else
+      return "#{value} of #{suit.to_s}"
+    end
+  end
+
+  private
+
+  def convert_facecard_value
+    value_hash = { 1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
+    return value_hash[value]
   end
 
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -26,9 +26,12 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
-
-      
-
+      (2..10).each do |i|
+        [:diamonds, :hearts, :spades, :clubs].each do |suit|
+          card = Card.new(i, suit)
+          expect(card.to_s).must_be_instance_of String
+        end
+      end
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -43,19 +46,46 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+      [:hearts, :diamonds, :spades, :clubs].each do |suit|
+        [1, 11, 12, 13].each do |value|
+          card = Card.new(value, suit)
+          case value
+          when 1
+            expect(card.to_s).must_equal "Ace of #{suit.to_s}"
+          when 11
+            expect(card.to_s).must_equal "Jack of #{suit.to_s}"
+          when 12
+            expect(card.to_s).must_equal "Queen of #{suit.to_s}"
+          when 13
+            expect(card.to_s).must_equal "King of #{suit.to_s}"
+          end
+        end
+      end
+
     end
   end
 
   describe "Reader methods" do
 
     it "Can retrieve the value of the card using a `.value`." do
-      # ensure that `.value works as expected`
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.value).must_be_instance_of Integer
+          expect(card.value).must_equal value
+        end
+      end
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.suit).must_be_instance_of Symbol
+          expect(card.suit).must_equal suit
+        end
+      end
     end
   end
-
 end
